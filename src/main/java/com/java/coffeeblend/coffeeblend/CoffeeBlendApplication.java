03d18ff4cae41c9945d5e8406206cdb5d94d8b6d
Hoanndpk01299 <nguyenduchoan972000@gmail.com>
2022-02-10 16:50:09 +0700

    Hoannd

---
+++ src/main/java/com/java/coffeeblend/coffeeblend/CoffeeBlendApplication.java
@@ -0,0 +1,13 @@
+package com.java.coffeeblend.coffeeblend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeBlendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeBlendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java/coffeeblend/coffeeblend/CoffeeBlendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.coffeeblend.coffeeblend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeBlendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
